e797dccbf1069ec2c9d505c5b7857361d96a259d
Amy Wallhermfechtel <wallheac@gmail.com>
2017-04-27 15:51:56 -0500

    modified javascript file to display badges

--- src/assets/main.js
+++ src/assets/main.js
@@ -1,5 +1,16 @@
 $(function() {
-
-  // your code will go here
-
+  $.ajax({
+    url: 'https://www.codeschool.com/users/wallheac.json',
+    dataType: 'jsonp',
+    success: function(response){
+      var elements = $.map(response.courses.completed, function(course, index){
+          var listItem = $('<div class="course"></div>');
+          $(listItem).append('<h3>' + course.title +'</h3>');
+          $(listItem).append('<img src=' + course.badge + '>');
+          $(listItem).append('<a href=' + course.url + ' target="_blank" class="btn btn-primary">See Course</a>');
+          return listItem;
+        });
+        $('#badges').html(elements);
+    }
+  });
 });
